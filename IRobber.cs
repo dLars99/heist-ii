606a0daaddad96037f34fb3d6fc8675695c0413c
David Larsen <davidplarsen@gmail.com>
2020-09-04 10:59:04 -0500

    Create IRobber interface

---
+++ IRobber.cs
@@ -0,0 +1,13 @@
+namespace HeistII
+{
+    public interface IRobber
+    {
+        string Name { get; set; }
+        int SkillLevel { get; set; }
+        int PercentageCut { get; set; }
+        void PerformSkill(Bank bank)
+        {
+
+        };
+    }
+}
