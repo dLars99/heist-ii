56ac27faa873bd973b65eb8eac5f162e3460c9e3
David Larsen <davidplarsen@gmail.com>
2020-09-04 11:26:16 -0500

    Build rolodex of unsavory fellows

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HeistII
 {
@@ -7,6 +8,51 @@ namespace HeistII
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<IRobber> rolodex = new List<IRobber>()
+            {
+                new Hacker
+                {
+                Name = "Brian",
+                SkillLevel = 27,
+                PercentageCut = 15
+                },
+                new Muscle
+                {
+                Name = "Skor",
+                SkillLevel = 62,
+                PercentageCut = 50
+                },
+                new LockSpecialist
+                {
+                Name = "The Cracken",
+                SkillLevel = 48,
+                PercentageCut = 33
+                },
+                new Hacker
+                {
+                Name = "H4xx0rz1337",
+                SkillLevel = 73,
+                PercentageCut = 65
+                },
+                new Muscle
+                {
+                Name = "Tommy",
+                SkillLevel = 42,
+                PercentageCut = 30
+                },
+                new LockSpecialist
+                {
+                Name = "Sticky Pete",
+                SkillLevel = 35,
+                PercentageCut = 20
+                }
+            };
+        }
+
+        static void TitleScreen()
+        {
+            Console.WriteLine("Heist II:");
+            Console.WriteLine("iRobbing");
         }
     }
 }
